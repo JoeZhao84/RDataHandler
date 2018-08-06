1bb6a87630bcd82184499975f5b1fe66c08e87eb
JoeZhao84 <JoeZhao84@users.noreply.github.com>
2018-08-06 12:06:27 +0100

    Update RDataHandler.r

--- RDataHandler.r
+++ RDataHandler.r
@@ -2,11 +2,13 @@
 library(dplyr)
 files <- list.files("C:/Work/DestSelect/ECDB/", pattern="*.RData")
  
- my.list <- vector(length(files),mode="list")
- for (i in 1:length(files)) {
+my.list <- vector(length(files),mode="list")
+
+for (i in 1:length(files)) {
    load(files[i])
    my.list[[i]] <- get("parent")
-}
+   }
+
 my.rbind <- do.call(rbind,my.list)
 
 write.csv(my.rbind, "C:/Work/DestSelect/ECDB/CSV/test1.csv")
